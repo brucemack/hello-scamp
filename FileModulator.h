338f043f9516015ac947173815b46834cc9fd0df
Bruce MacKinnon <bruce@mackinnon.com>
2023-10-29 21:43:54 -0400

    Added comments

--- FileModulator.h
+++ FileModulator.h
@@ -1,3 +1,19 @@
+/*
+SCAMP Encoder/Decoder
+Copyright (C) 2023 - Bruce MacKinnon KC1FSZ
+
+This program is free software: you can redistribute it and/or modify it under 
+the terms of the GNU General Public License as published by the Free 
+Software Foundation, either version 3 of the License, or (at your option) any 
+later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT 
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with 
+this program. If not, see <https://www.gnu.org/licenses/>.
+*/
 #ifndef _FileModulator_h
 #define _FileModulator_h
 
@@ -6,6 +22,9 @@
 
 namespace scamp {
 
+    /**
+     * A modulator that writes baseband data to a text file.
+     */
     class FileModulator : public Modulator {
     public:
 
@@ -26,7 +45,6 @@ namespace scamp {
         unsigned int _markFreq;
         unsigned int _spaceFreq;
     };
-
 }
 
 #endif
--- Modulator.h
+++ Modulator.h
@@ -1,7 +1,28 @@
+/*
+SCAMP Encoder/Decoder
+Copyright (C) 2023 - Bruce MacKinnon KC1FSZ
+
+This program is free software: you can redistribute it and/or modify it under 
+the terms of the GNU General Public License as published by the Free 
+Software Foundation, either version 3 of the License, or (at your option) any 
+later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT 
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with 
+this program. If not, see <https://www.gnu.org/licenses/>.
+*/
 #ifndef _Modulator_h
 #define _Modulator_h
 
 namespace scamp {
+
+    /**
+     * A generic interface for a modulator that would be driven by 
+     * an encoder.
+     */
     class Modulator {
     public:
 
